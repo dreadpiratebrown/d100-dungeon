7f365eca9acb7520e9c41a613d97aee11ba6264f
joshuadbrown <dreadpiratebrown@gmail.com>
2024-06-19 19:43:33 -0400

    Files not added

---
+++ src/store/map.js
@@ -0,0 +1,23 @@
+export const useMapStore = (set) => ({
+  mapTiles: [],
+  locations: [],
+  exits: [],
+  addMapTile: (tile) =>
+    set((state) => ({
+      mapTiles: [...state.mapTiles, tile],
+    })),
+  addLocation: (location) =>
+    set((state) => ({
+      locations: [...state.locations, location],
+    })),
+  addExit: (exit) =>
+    set((state) => ({
+      exits: [...state.exits, exit],
+    })),
+  resetMap: () =>
+    set({
+      mapTiles: [],
+      locations: [],
+      exits: [],
+    }),
+});
